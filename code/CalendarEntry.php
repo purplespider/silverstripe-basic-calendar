34220e3ad4767d499dbf9e2c610e46d31ee37598
spekulatius <petert@catalyst.net.nz>
2015-12-13 19:17:08 +1300

    Converted to PSR-2

--- code/CalendarEntry.php
+++ code/CalendarEntry.php
@@ -1,106 +1,112 @@
 <?php
 
-class CalendarEntry extends DataObject{
+class CalendarEntry extends DataObject
+{
 
- 	public static $db = array(
- 		"Title" => "Text",
- 		"Date" => "Date",
- 		"Time" => "Text",
- 		"Description" => "Text"
- 	);
-
- 	static $has_one = array(
- 		"CalendarPage" => "CalendarPage",
- 		"Image" => "Image"
- 	);
-
-	public static $summary_fields = array(
-    	"Date" => "Date",
-    	"Title" => "Title"
+    public static $db = array(
+        "Title" => "Text",
+        "Date" => "Date",
+        "Time" => "Text",
+        "Description" => "Text"
     );
 
-	static $default_sort = "Date ASC, Time ASC";
+    public static $has_one = array(
+        "CalendarPage" => "CalendarPage",
+        "Image" => "Image"
+    );
 
- 	public function validate() {
+    public static $summary_fields = array(
+        "Date" => "Date",
+        "Title" => "Title"
+    );
+
+    public static $default_sort = "Date ASC, Time ASC";
+
+    public function validate()
+    {
         $result = parent::validate();
-        if(!$this->Title) {
+        if (!$this->Title) {
             $result->error('Title is required');
         }
-        if(!$this->Date) {
+        if (!$this->Date) {
             $result->error('Date is required');
         }
         return $result;
     }
 
- 	function getCMSFields() {
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            $datefield = new DateField('Date', 'Date (DD/MM/YYYY)*');
+            $datefield->setConfig('showcalendar', true);
+            $datefield->setConfig('dateformat', 'dd/MM/YYYY');
 
-		$this->beforeUpdateCMSFields(function($fields) {
-			$datefield = new DateField('Date','Date (DD/MM/YYYY)*');
-			$datefield->setConfig('showcalendar', true);
-			$datefield->setConfig('dateformat', 'dd/MM/YYYY');
+            $imagefield = new UploadField('Image', 'Image');
+            $imagefield->allowedExtensions = array('jpg', 'gif', 'png');
+            $imagefield->setFolderName("Managed/CalendarImages");
+            $imagefield->setCanPreviewFolder(false);
 
-			$imagefield = new UploadField('Image','Image');
-			$imagefield->allowedExtensions = array('jpg', 'gif', 'png');
-			$imagefield->setFolderName("Managed/CalendarImages");
-			$imagefield->setCanPreviewFolder(false);
+            $fields->addFieldToTab('Root.Main', new TextField('Title', "Event Title*"));
+            $fields->addFieldToTab('Root.Main', $datefield);
+            $fields->addFieldToTab('Root.Main', new TextField('Time', "Time (HH:MM)"));
+            $fields->addFieldToTab('Root.Main', new TextareaField('Description'));
+            $fields->addFieldToTab('Root.Main', $imagefield);
+        });
 
-			$fields->addFieldToTab('Root.Main', new TextField('Title',"Event Title*"));
-			$fields->addFieldToTab('Root.Main', $datefield);
-			$fields->addFieldToTab('Root.Main', new TextField('Time',"Time (HH:MM)"));
-			$fields->addFieldToTab('Root.Main', new TextareaField('Description'));
-			$fields->addFieldToTab('Root.Main', $imagefield);
-		});
+        $fields = parent::getCMSFields();
 
-		$fields = parent::getCMSFields();
+        $this->extend('updateCMSFields', $fields);
 
-		$this->extend('updateCMSFields', $fields);
+        $fields->removeFieldFromTab("Root.Main", "CalendarPageID");
 
-		$fields->removeFieldFromTab("Root.Main","CalendarPageID");
+        return $fields;
+    }
 
-		return $fields;
-	}
+    public function getMonthDigit()
+    {
+        $date = strtotime($this->Date);
+        return date('m', $date);
+    }
 
-	function getMonthDigit() {
-	 	$date = strtotime($this->Date);
-		return date('m',$date);
-	}
+    public function getYear()
+    {
+        $date = strtotime($this->Date);
+        return date('Y', $date);
+    }
 
-	function getYear() {
-		$date = strtotime($this->Date);
-		return date('Y',$date);
-	}
+    public function canCreate($members = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $members);
+        if ($extended !== null) {
+            return $extended;
+        }
+        return true;
+    }
 
-	function canCreate($members = null) {
-		$extended = $this->extendedCan(__FUNCTION__, $members);
-		if($extended !== null) {
-			return $extended;
-		}
-		return true;
-	}
+    public function canEdit($members = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $members);
+        if ($extended !== null) {
+            return $extended;
+        }
+        return true;
+    }
 
-	function canEdit($members = null) {
-		$extended = $this->extendedCan(__FUNCTION__, $members);
-		if($extended !== null) {
-			return $extended;
-		}
-		return true;
-	}
+    public function canDelete($members = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $members);
+        if ($extended !== null) {
+            return $extended;
+        }
+        return true;
+    }
 
-	function canDelete($members = null) {
-		$extended = $this->extendedCan(__FUNCTION__, $members);
-		if($extended !== null) {
-			return $extended;
-		}
-		return true;
-	}
-
-	function canView($members = null) {
-		$extended = $this->extendedCan(__FUNCTION__, $members);
-		if($extended !== null) {
-			return $extended;
-		}
-		return true;
-	}
-
+    public function canView($members = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $members);
+        if ($extended !== null) {
+            return $extended;
+        }
+        return true;
+    }
 }
-?>
